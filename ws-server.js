e4aad8808b8969c597c788b284e3c9c43e092d04
arekzaluski <arkadiusz.zaluski@arm.com>
2015-12-23 09:46:42 +0000

    Add node.js websocket server

---
+++ ws-server.js
@@ -0,0 +1,64 @@
+//Use optimist to parse input arguments
+var argv = require('optimist')
+    .usage('Usage: ws-server -p [num] \n')
+    .demand('p')
+    .alias('p','port')
+    .describe('p', 'port to open webserver on')
+    .argv;
+
+//Create web server and open it at specific port 
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ port: argv.p });
+
+var clients=[];
+var maxClients=2;
+var storedMessage = null;
+
+wss.on('connection', function connection(client) {
+  if(clients.length >= maxClients) {
+    console.log("Cannot connect more clients");
+    return;
+  }
+  console.log("websocket connection opened");
+  clients.push(client);
+  client.pingssent = 0;
+  var interval = setInterval(function() {
+      client.ping();
+      client.pingssent++;
+  }, 10*1000);// 10 seconds between pings
+
+  client.on("pong", function() { // we received a pong from the client.
+    client.pingssent = 0; // reset ping counter.
+  });
+
+  client.on('message', function incoming(message) {
+    console.log("Message received");
+    if(clients.length ==1) {
+      storedMessage = message;
+      console.log("Message will be stored because second client is not connected.");
+      return;
+    }
+    for(var i=0;i<clients.length;i++) {
+      if(clients[i] != client) {
+        clients[i].send(message);
+      }
+    }
+  });
+  client.on('close', function closeSocket() {
+    var index = clients.indexOf(client);
+    clearInterval(interval);
+    if (index > -1) {
+      console.log("Client disconnected");
+      clients.splice(index, 1);
+    }
+  });
+  client.on('error', function socketError() {
+    console.log("connection has error");
+    client.close();
+  });
+  //message was sent before second client connected.
+  if(clients.length == maxClients && storedMessage != null) {
+    client.send(storedMessage);
+    storedMessage = null;
+  }
+});
